3e9b2061685aba9b4cb4db81facf8e8af3e024f0
wen2811 <wenbehr@gmail.com>
2023-07-15 16:53:41 +0200

    aanpassingen gemaakt

--- conditionals.js
+++ conditionals.js
@@ -8,7 +8,7 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false wanneer gelogd
 // ==========================================
-
+const
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true wanneer gelogd
